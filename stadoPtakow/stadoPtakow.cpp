717ea466e90c1cb3c8d83e04f6b4a7a4b4a0b7d4
jozuel <jozuel@gmail.com>
2017-01-17 22:32:39 +0100

    dodanie obslugi vectora i pobieranie z plikow

--- stadoPtakow/stadoPtakow.cpp
+++ stadoPtakow/stadoPtakow.cpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <time.h>
 
 #define M_PI 3.14159265358979323846
 
@@ -62,9 +63,9 @@ private:
 	bool czyGlodny;
 	bool czywStadzie;
 	bool ucieczka;
-	double predkosc;
-	//double zasiegWidzenia;
+	double predkosc;	///losowa
 	orientacja zwrot;
+	Obiekt *najblizszyOsobnik;
 public:
 	Osobnik():Obiekt()
 	{
@@ -73,7 +74,7 @@ public:
 		ucieczka = false;
 		predkosc = 1;
 		zwrot = lewo;
-		Obiekt *najblizszyOsobnik;
+		
 	}
 	Osobnik(bool glod, bool stado, bool uciekaj, double v, orientacja orient, double x, double y) : Obiekt(x, y)
 	{
@@ -82,6 +83,20 @@ public:
 		ucieczka = uciekaj;
 		predkosc = v;
 		zwrot = orient;
+	}
+	Osobnik(double x, double y) : Obiekt(x, y)	//podstawowe losowanie parametrów przy pobieranu danych z pliku
+	{
+		int losowo;
+		srand(time(NULL));
+		losowo = rand() % 4 + 1;	//od 1 do 4
+		losowo > 1 ? czyGlodny = true : czyGlodny = false;
+		czywStadzie = false;
+		ucieczka = false;
+		losowo = rand() % 4;
+		zwrot = orientacja(losowo);
+		losowo = rand() % 800 + 100; //prêdkoœæ przelotowa jaskó³ki to 32km/h czyli okolo 8,8 m/s
+		losowo /= 100;
+		predkosc = losowo;
 
 	}
 	~Osobnik()
@@ -184,6 +199,36 @@ public:
 	}
 
 };
+Obiekt* utworzObiekt(double posX, double posY, char typObiektu)
+{
+	Obiekt* tmp = nullptr;
+	try
+	{
+		switch (typObiektu)
+		{
+		case 'O':
+			tmp = new Osobnik(posX, posY);
+			break;
+		case 'J':
+			tmp = new Osobnik(posX, posY);
+			//tmp = new Jedzenie(posX, posY);
+			break;
+		case 'D':
+			tmp = new Osobnik(posX, posY);
+			//tmp = new Drapieznik(posX, posY);
+			break;
+		default:
+			throw "bledny obiekt";
+			break;
+		}
+		return tmp;
+	}
+	catch (const std::exception&)
+	{
+		
+	}
+	
+}
 double zmianaNaRadiany(int stopnie)
 {
 	return ((stopnie * M_PI) / 180);
@@ -196,9 +241,10 @@ void wypelnijTangens(double tab[], int n)		//oblicza tangens w stopniach
 		//cout <<i<<" " <<tab[i] << endl;
 	}
 }
-string* rozszezanieTablicy(string *tab, int rozmiar)
+template <typename T>			//szablon do rozszezania tablicy 
+T* rozszezanieTablicy(T *tab, int rozmiar)
 {
-	string *newTab = new string[rozmiar + 1];
+	T *newTab = new T[rozmiar + 1];
 	for (int i = 0; i < rozmiar; i++)
 	{
 		newTab[i] = tab[i];
@@ -206,23 +252,76 @@ string* rozszezanieTablicy(string *tab, int rozmiar)
 	delete[] tab;
 	return newTab;
 }
-void pobieraniezPliku()
+//string* rozszezanieTablicy(string *tab, int rozmiar)
+//{
+//	string *newTab = new string[rozmiar + 1];
+//	for (int i = 0; i < rozmiar; i++)
+//	{
+//		newTab[i] = tab[i];
+//	}
+//	delete[] tab;
+//	return newTab;
+//}
+void ustawienieObiektow(string *&linie, int iloscLini,vector<Obiekt*> &obiekty)
+{
+	double posX, posY;	
+	Obiekt *tmp;
+	string liczba = "";
+	char typObiektu;
+	int iloscObiektow = 0;
+	//bool znalezionoPrzecinek = false;
+	for (int i = 0; i < iloscLini; i++)		
+	{
+		typObiektu = linie[i][0];
+		for (int j = 0; j < linie[i].length(); j++)
+		{
+			if (isdigit(linie[i][j]) || (linie[i][j] == '.' ))	//jesli czesc znaku jest liczba lub kropka
+			{
+				liczba += linie[i].at(j);
+			}
+			else if (linie[i][j] == ',')
+			{
+				//znalezionoPrzecinek = true;	//czy potrzebna jest zmienna znalezionoPrzecinek?
+				posX = stod(liczba);	//stod(string) do zmiany stringa na liczbe
+				liczba = "";
+			}
+			else if(linie[i][j] == ')')	//mamy 2 liczbe
+			{
+				posY = stod(liczba);
+				liczba = "";
+				//rozszezanieTablicy <Obiekt> (obiekty,iloscObiektow);
+				iloscObiektow++;
+				obiekty.push_back(utworzObiekt(posX, posY, typObiektu));
+				//obiekty[iloscObiektow] = tmp;
+				//utworzObiekt(posX,posY,typObiektu);
+				//cout << " " << posX << " " << posY << endl;		//dziala nawet dla liczb zmiennoprzecinkowych
+				//znalezionoPrzecinek = false;
+			}
+
+		}
+	}
+}
+void pobieraniezPliku(string *&linie,int &iloscLini)
 {
 	ifstream plik;
-	string *linie;
-	int iloscLini = 0;
+	//string *linie;
+	double liczba = 0;
+	//int iloscLini = 0;
 	linie = new string[1];
 	plik.open("stadoPtakow.txt");	//zmienic potem na parametr
 	if (plik)
 	{
 		while (!plik.eof())
 		{
+			
+			linie=rozszezanieTablicy(linie, iloscLini);
+			getline(plik, linie[iloscLini]);	//pobiera cala linie
+			//plik >> linie[iloscLini - 1];
+			cout << linie[iloscLini]<<endl;
 			iloscLini++;
-			linie=rozszezanieTablicy(linie, iloscLini - 1);
-			getline(plik, linie[iloscLini - 1]);	//pobiera cala linie
-			cout << linie[iloscLini-1]<<endl;
 		}
 		plik.close();
+		//ustawienieObiektow(linie, iloscLini);
 	}
 	else
 	{
@@ -235,13 +334,34 @@ int main()
 	float maxX;
 	float maxY;
 	double stopnie[90];
-	int n = 90;
-	double zasiegWidzenia = 90;
-	Obiekt *obiekty;
-	obiekty = new Osobnik();
-	cout << obiekty[0].ilosc << endl;
-	wypelnijTangens(stopnie, n);
-	pobieraniezPliku();
+	int iloscLini = 0;
+	string* linieTekstu;
+	//int n = 90;
+	vector< Obiekt* > listaObiektow;		//chyba bedzie lepiej uzywac tablicy obiektow
+	//parametry startowe z lini polecen
+		string plikWejsciowy;
+		double zasiegWidzenia = 90;
+		int katWidzenia;	//od 0 do 90
+		float rozmiarOsobnika;
+		char trybPracy;
+		string plikWyjsciowy;
+	//end parametry
+	//Obiekt *obiekty;
+	//obiekty = new Osobnik[1]();
+		listaObiektow.push_back(new Osobnik());
+		//cout<<listaObiektow[0]->getX();
+		
+	//cout << listaObiektow[0].ilosc << endl;
+	wypelnijTangens(stopnie, 90);
+	pobieraniezPliku(linieTekstu,iloscLini);
+	ustawienieObiektow(linieTekstu, iloscLini, listaObiektow);	//nie zapomniec o zwolnieniu pamieci
+	for (int i = 0; i < listaObiektow.size(); i++)
+	{
+		cout << listaObiektow[i]->getX() << " " << listaObiektow[i]->getY() << endl;		//ok dziala vector
+		delete listaObiektow[i];
+	}
+	listaObiektow.clear();
+	//delete listaObiektow[0];
 	getchar();
 	return 0;
 }
